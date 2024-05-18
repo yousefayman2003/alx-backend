ae28cad3cda0aaf91b003541379579d2a5b2b3ff
Yousef Ayman <yousefayman20003@gmail.com>
2024-05-18 11:08:07 -0700

    Second task done

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""Module that implements get_page method inside `Server` class"""
+import csv
+import math
+from typing import List
+
+
+index_range = __import__('0-simple_helper_function').index_range
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """gets data for a page"""
+        assert isinstance(page, int)
+        assert isinstance(page_size, int)
+        assert page > 0
+        assert page_size > 0
+
+        start_index, end_index = index_range(page, page_size)
+
+        dataset = self.dataset()
+        if start_index > len(self.__dataset) or \
+                end_index > len(self.__dataset):
+            return []
+
+        return self.__dataset[start_index: end_index]
